78a87fcf81ad4f13a07efbd0b66ee2ed1bc9a704
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-30 19:44:02 -0300

    feat: driver controller

--- internal/controllers/driver_controller.go
+++ internal/controllers/driver_controller.go
@@ -1,1 +1,44 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/vtx-account-manager/internal/middleware"
+	"github.com/venture-technology/vtx-account-manager/internal/service"
+)
+
+type DriverController struct {
+	driverservice *service.DriverService
+}
+
+func NewDriverController(driverservice *service.DriverService) *DriverController {
+	return &DriverController{
+		driverservice: driverservice,
+	}
+}
+
+func (ct *DriverController) RegisterRoutes(router *gin.Engine) {
+
+	api := router.Group("api/v1/vtx-account-manager")
+
+	api.GET("/school", middleware.DriverMiddleware(), ct.GetSchool)   // para visualizar todas as suas escolas
+	api.GET("/sponsor", middleware.DriverMiddleware(), ct.GetSponsor) // para visualizar todos os sponsors
+	api.GET(":cnh/school", func(c *gin.Context) {
+		school := c.Query("school")
+		if school == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "The param 'school' not found"})
+		}
+	}, ct.IsSchool)
+}
+
+func (ct *DriverController) GetSchool(c *gin.Context) {
+}
+
+func (ct *DriverController) GetSponsor(c *gin.Context) {
+
+}
+
+func (ct *DriverController) IsSchool(c *gin.Context) {
+
+}
