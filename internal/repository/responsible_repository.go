1ba85b1340f997ba425ae301a5ffe0873e7caae2
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-30 19:45:12 -0300

    feat: school and responsible repository

--- internal/repository/responsible_repository.go
+++ internal/repository/responsible_repository.go
@@ -1,1 +1,20 @@
 package repository
+
+import "database/sql"
+
+type IResponsibleRepository interface {
+	FindAllDriverAtSchool()
+	CreateSponsor()
+	GetPartners()
+	BreachSponsor()
+}
+
+type ResponsibleRepository struct {
+	db *sql.DB
+}
+
+func NewResponsibleRepository(db *sql.DB) *ResponsibleRepository {
+	return &ResponsibleRepository{
+		db: db,
+	}
+}
--- internal/repository/school_repository.go
+++ internal/repository/school_repository.go
@@ -1,1 +1,18 @@
 package repository
+
+import "database/sql"
+
+type ISchoolRepository interface {
+	GetDriver()
+	GetSponsors()
+}
+
+type SchoolRepository struct {
+	db *sql.DB
+}
+
+func NewSchoolRepository(db *sql.DB) *SchoolRepository {
+	return &SchoolRepository{
+		db: db,
+	}
+}
