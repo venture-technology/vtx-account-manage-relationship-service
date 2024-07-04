3feb476f80acb5865c601e8ecccf66f5866d5168
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-04 17:55:43 -0300

    feat: creating funcs of service

--- internal/service/driver_service.go
+++ internal/service/driver_service.go
@@ -1,6 +1,11 @@
 package service
 
-import "github.com/venture-technology/vtx-account-manager/internal/repository"
+import (
+	"context"
+
+	"github.com/venture-technology/vtx-account-manager/internal/repository"
+	"github.com/venture-technology/vtx-account-manager/models"
+)
 
 type DriverService struct {
 	driverrepository repository.IDriverRepository
@@ -10,4 +15,12 @@ func NewDriverService(repo repository.IDriverRepository) *DriverService {
 	return &DriverService{
 		driverrepository: repo,
 	}
+}
+
+func (ds *DriverService) GetSchool(ctx context.Context, cnh *string) ([]models.School, error) {
+	return ds.driverrepository.GetSchool(ctx, cnh)
+}
+
+func (ds *DriverService) GetSponsors(ctx context.Context, cnpj *string) ([]models.Sponsor, error) {
+	return ds.driverrepository.GetSponsors(ctx, cnpj)
 }
--- internal/service/responsible_service.go
+++ internal/service/responsible_service.go
@@ -1,6 +1,11 @@
 package service
 
-import "github.com/venture-technology/vtx-account-manager/internal/repository"
+import (
+	"context"
+
+	"github.com/venture-technology/vtx-account-manager/internal/repository"
+	"github.com/venture-technology/vtx-account-manager/models"
+)
 
 type ResponsibleService struct {
 	responsiblerepository repository.IResponsibleRepository
@@ -10,4 +15,20 @@ func NewResponsibleService(repo repository.IResponsibleRepository) *ResponsibleS
 	return &ResponsibleService{
 		responsiblerepository: repo,
 	}
+}
+
+func (rs *ResponsibleService) CreateSponsor(ctx context.Context, sponsor *models.Sponsor) error {
+	return rs.responsiblerepository.CreateSponsor(ctx, sponsor)
+}
+
+func (rs *ResponsibleService) GetPartners(ctx context.Context, cpf *string) ([]models.Sponsor, error) {
+	return rs.responsiblerepository.GetPartners(ctx, cpf)
+}
+
+func (rs *ResponsibleService) FindAllDriverAtSchool(ctx context.Context, cnpj *string) ([]models.Driver, error) {
+	return rs.responsiblerepository.FindAllDriverAtSchool(ctx, cnpj)
+}
+
+func (rs *ResponsibleService) BreachSponsor(ctx context.Context, record *int) error {
+	return rs.responsiblerepository.BreachSponsor(ctx, record)
 }
