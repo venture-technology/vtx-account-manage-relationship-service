b0dbe1bd9b7b2618af29cdf7a941cf1b955b6dc3
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-26 19:18:48 -0300

    feat: add address to calc distance

--- models/driver.go
+++ models/driver.go
@@ -1,7 +1,11 @@
 package models
 
 type Driver struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	CNH   string `json:"cnh"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	CNH        string `json:"cnh"`
+	Street     string `json:"street"`
+	Number     string `json:"number"`
+	Complement string `json:"complement"`
+	ZIP        string `json:"zip"`
 }
--- models/school.go
+++ models/school.go
@@ -1,7 +1,11 @@
 package models
 
 type School struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	CNPJ  string `json:"cnpj"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	CNPJ       string `json:"cnpj"`
+	Street     string `json:"street"`
+	Number     string `json:"number"`
+	Complement string `json:"complement"`
+	ZIP        string `json:"zip"`
 }
