5fdd90ccf97fe4e0b0df1aed64ebf55193d11dfb
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-29 12:39:01 -0300

    feat: new fields in contracts

--- database/schema.sql
+++ database/schema.sql
@@ -11,15 +11,26 @@ CREATE TABLE IF NOT EXISTS partners (
 );
 
 -- Tabela de account-manager
-CREATE TABLE IF NOT EXISTS sponsors (
+CREATE TABLE IF NOT EXISTS contracts (
     record SERIAL PRIMARY KEY,
+    title_stripe_subscription TEXT NOT NULL,
+    id_stripe_subscription TEXT NOT NULL,
+    id_price_subscription TEXT NOT NULL,
+    id_product_subscription TEXT NOT NULL,
     name_school VARCHAR(100) NOT NULL,
     cnpj_school VARCHAR(14) NOT NULL,
     email_school VARCHAR(100) NOT NULL,
+    street_school TEXT NOT NULL,
+    number_school TEXT NOT NULL,
+    zip_school TEXT NOT NULL,
     name_driver VARCHAR(100) NOT NULL,
     cnh_driver VARCHAR(14) NOT NULL,
     email_driver VARCHAR(100) NOT NULL,
+    street_driver TEXT NOT NULL,
+    number_driver TEXT NOT NULL,
+    zip_driver TEXT NOT NULL,
     cpf_responsible VARCHAR(11) NOT NULL,
+    custom_id_responsible TEXT NOT NULL,
     name_responsible VARCHAR(100) NOT NULL,
     email_responsible VARCHAR(100) NOT NULL,
     street_responsible TEXT NOT NULL,
@@ -29,5 +40,8 @@ CREATE TABLE IF NOT EXISTS sponsors (
     name_child VARCHAR(50) NOT NULL,
     rg_child VARCHAR(10) NOT NULL,
     shift VARCHAR(14) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    back_url TEXT NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expire_at TEXT NOT NULL,
+    status TEXT NOT NULL
 );
