31ba5d200fe5f8196489988663322a0e9dd9ff64
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-08-05 17:19:09 -0300

    feat: testing business roles

---
+++ internal/service/stripe_contract_service_test.go
@@ -0,0 +1,27 @@
+package service
+
+import "testing"
+
+func TestGetDistance(t *testing.T) {}
+
+func TestCalculateValueSubscription(t *testing.T) {}
+
+func TestCreateProduct(t *testing.T) {}
+
+func TestCreatePrice(t *testing.T) {}
+
+func TestCreateSubscription(t *testing.T) {}
+
+func TestListSubscriptions(t *testing.T) {}
+
+func TestGetSubscription(t *testing.T) {}
+
+func TestDeleteSubscription(t *testing.T) {}
+
+func TestGetInvoice(t *testing.T) {}
+
+func TestListInvoices(t *testing.T) {}
+
+func TestCalculateRemainingValueSubscription(t *testing.T) {}
+
+func TestFineResponsible(t *testing.T) {}
