d498173d3e95b5b037118a7d5a9cb3d69b59cc17
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-08-06 22:17:16 -0300

    feat: create new funcs of repo

--- internal/repository/responsible_repository.go
+++ internal/repository/responsible_repository.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/google/uuid"
 	"github.com/venture-technology/vtx-account-manager/models"
 )
 
 type IResponsibleRepository interface {
 	FindAllDriverAtSchool(ctx context.Context, cnpj *string) ([]models.Driver, error)
 	CreateContract(ctx context.Context, contract *models.Contract) error
-	GetPartners(ctx context.Context, cpf *string) ([]models.Contract, error)
-	DeleteContract(ctx context.Context, record *int) error
+	FindContractsByCpf(ctx context.Context, cpf, status *string) ([]models.Contract, error)
+	DeleteContract(ctx context.Context, record uuid.UUID) error
+	ExpireContract(ctx context.Context, record uuid.UUID) error
 }
 
 type ResponsibleRepository struct {
@@ -27,35 +29,63 @@ func NewResponsibleRepository(db *sql.DB) *ResponsibleRepository {
 func (rr *ResponsibleRepository) CreateContract(ctx context.Context, contract *models.Contract) error {
 
 	sqlQuery := `INSERT INTO contracts (
-		name_driver, 
-		cnh_driver, 
-		email_driver, 
-		name_school, 
-		cnpj_school, 
-		email_school, 
-		name_responsible, 
-		cpf_responsible, 
-		email_responsible, 
-		street_responsible, 
-		number_responsible, 
-		complement_responsible, 
-		zip_responsible, 
-		name_child, 
-		rg_child, 
-		shift, 
-		created_at
+		record,
+		title_stripe_subscription,
+		description_stripe_subscription,
+		id_stripe_subsciption,
+		id_price_subscription,
+		id_product_subscription,
+		name_school,
+		cnpj_school,
+		email_school,
+		street_school,
+		complement_school,
+		zip_school,
+		name_driver,
+		cnh_driver,
+		email_driver,
+		street_driver,
+		complement_driver,
+		zip_driver,
+		name_responsible,
+		cpf_responsible,
+		customer_id_responsible,
+		payment_method_id_responsible,
+		email_responsible,
+		street_responsible,
+		complement_responsible,
+		zip_responsible,
+		name_child,
+		rg_child,
+		shift,
+		expire_at,
+		status
 	)
-	VALUES ($1, $2. $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)`
+	VALUES ($1, $2. $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, "currently")`
 
 	_, err := rr.db.Exec(sqlQuery,
-		contract.Driver.Name,
-		contract.Driver.CNH,
-		contract.Driver.Email,
+		contract.Record,
+		contract.StripeSubscription.Title,
+		contract.Description,
+		contract.StripeSubscription.SubscriptionId,
+		contract.StripeSubscription.PriceSubscriptionId,
+		contract.StripeSubscription.ProductSubscriptionId,
 		contract.School.Name,
 		contract.School.CNPJ,
 		contract.School.Email,
+		contract.School.Street,
+		contract.School.Complement,
+		contract.School.ZIP,
+		contract.Driver.Name,
+		contract.Driver.CNH,
+		contract.Driver.Email,
+		contract.Driver.Street,
+		contract.Driver.Complement,
+		contract.Driver.ZIP,
 		contract.Child.Responsible.Name,
 		contract.Child.Responsible.CPF,
+		contract.Child.Responsible.CustomerId,
+		contract.Child.Responsible.PaymentMethodId,
 		contract.Child.Responsible.Email,
 		contract.Child.Responsible.Street,
 		contract.Child.Responsible.Complement,
@@ -63,35 +93,50 @@ func (rr *ResponsibleRepository) CreateContract(ctx context.Context, contract *m
 		contract.Child.Name,
 		contract.Child.RG,
 		contract.Child.Shift,
-		contract.CreatedAt,
+		contract.ExpireAt,
 	)
 
 	return err
 
 }
 
-func (rr *ResponsibleRepository) GetPartners(ctx context.Context, cpf *string) ([]models.Contract, error) {
+func (rr *ResponsibleRepository) FindContractsByCpf(ctx context.Context, cpf, status *string) ([]models.Contract, error) {
 
 	sqlQuery := `SELECT 
-		name_driver, 
-		cnh_driver, 
-		email_driver, 
-		name_school, 
-		cnpj_school, 
-		email_school, 
-		name_responsible, 
-		cpf_responsible, 
-		email_responsible, 
-		street_responsible, 
-		number_responsible, 
-		complement_responsible, 
-		zip_responsible, 
-		name_child, 
-		rg_child, 
-		shift, 
-		created_at FROM contracts WHERE cpf_responsible = $1`
+		record,
+		title_stripe_subscription,
+		description_stripe_subscription,
+		id_stripe_subsciption,
+		id_price_subscription,
+		id_product_subscription,
+		name_school,
+		cnpj_school,
+		email_school,
+		street_school,
+		complement_school,
+		zip_school,
+		name_driver,
+		cnh_driver,
+		email_driver,
+		street_driver,
+		complement_driver,
+		zip_driver,
+		name_responsible,
+		cpf_responsible,
+		customer_id_responsible,
+		payment_method_id_responsible,
+		email_responsible,
+		street_responsible,
+		complement_responsible,
+		zip_responsible,
+		name_child,
+		rg_child,
+		shift,
+		created_at,
+		expire_at, 
+		status FROM contracts WHERE cpf_responsible = $1 AND status = $2`
 
-	rows, err := rr.db.Query(sqlQuery, cpf)
+	rows, err := rr.db.Query(sqlQuery, *cpf, *status)
 
 	if err != nil {
 		return nil, err
@@ -104,14 +149,28 @@ func (rr *ResponsibleRepository) GetPartners(ctx context.Context, cpf *string) (
 		var contract models.Contract
 
 		err := rows.Scan(
-			&contract.Driver.Name,
-			&contract.Driver.CNH,
-			&contract.Driver.Email,
+			&contract.Record,
+			&contract.StripeSubscription.Title,
+			&contract.Description,
+			&contract.StripeSubscription.SubscriptionId,
+			&contract.StripeSubscription.PriceSubscriptionId,
+			&contract.StripeSubscription.ProductSubscriptionId,
 			&contract.School.Name,
 			&contract.School.CNPJ,
 			&contract.School.Email,
+			&contract.School.Street,
+			&contract.School.Complement,
+			&contract.School.ZIP,
+			&contract.Driver.Name,
+			&contract.Driver.CNH,
+			&contract.Driver.Email,
+			&contract.Driver.Street,
+			&contract.Driver.Complement,
+			&contract.Driver.ZIP,
 			&contract.Child.Responsible.Name,
 			&contract.Child.Responsible.CPF,
+			&contract.Child.Responsible.CustomerId,
+			&contract.Child.Responsible.PaymentMethodId,
 			&contract.Child.Responsible.Email,
 			&contract.Child.Responsible.Street,
 			&contract.Child.Responsible.Complement,
@@ -120,6 +179,8 @@ func (rr *ResponsibleRepository) GetPartners(ctx context.Context, cpf *string) (
 			&contract.Child.RG,
 			&contract.Child.Shift,
 			&contract.CreatedAt,
+			&contract.ExpireAt,
+			&contract.Status,
 		)
 
 		if err != nil {
@@ -139,7 +200,7 @@ func (rr *ResponsibleRepository) GetPartners(ctx context.Context, cpf *string) (
 
 func (rr *ResponsibleRepository) FindAllDriverAtSchool(ctx context.Context, cnpj *string) ([]models.Driver, error) {
 
-	sqlQuery := `SELECT record, name_driver, cnh_driver, email_driver FROM partners WHERE cnpj_school = $1`
+	sqlQuery := `SELECT name_driver, cnh_driver, email_driver FROM partners WHERE cnpj_school = $1`
 
 	rows, err := rr.db.Query(sqlQuery, cnpj)
 
@@ -174,7 +235,7 @@ func (rr *ResponsibleRepository) FindAllDriverAtSchool(ctx context.Context, cnpj
 
 }
 
-func (rr *ResponsibleRepository) DeleteContract(ctx context.Context, record *int) error {
+func (rr *ResponsibleRepository) DeleteContract(ctx context.Context, record uuid.UUID) error {
 
 	tx, err := rr.db.Begin()
 	if err != nil {
@@ -191,6 +252,14 @@ func (rr *ResponsibleRepository) DeleteContract(ctx context.Context, record *int
 		}
 	}()
 	_, err = tx.Exec("DELETE FROM contracts WHERE record = $1", record)
+	return err
+
+}
+
+func (rr *ResponsibleRepository) ExpireContract(ctx context.Context, record uuid.UUID) error {
+
+	sqlQuery := `UPDATE contracts SET status = $1 WHERE record = $2`
+	_, err := rr.db.ExecContext(ctx, sqlQuery, "expired")
 	return err
 
 }
